a35154df550ce5e28a67b30a3543ade00fab6497
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-05-11 17:47:19 -0700

    script to build latest nccl

--- infra/build_nccl.sh
+++ infra/build_nccl.sh
@@ -4,10 +4,11 @@
 # - copies nccl libs to /usr/lib and /usr/local/cuda/lib, doesn't change nccl.2 symlinks
 
 # NCCL locations
-# /usr/lib/libnccl*   # default examples?
+# /usr/lib/libnccl*             # default examples?
 # /usr/local/cuda/lib/libncc*   # stock pytorch
-# ~/nccl/build    # build from source
-# $NCCL_HOME      # used by make
+# ~/nccl/build/lib    # build from source
+# $NCCL_HOME      # used by make for nccl-examples
+# $NCCL_ROOT_DIR  # used by PyTorch
 
 
 # NCCL links to /usr/local/cuda, make sure it's cuda-10.0
@@ -20,14 +21,15 @@ make -j src.build
 sudo apt install -y build-essential devscripts debhelper
 make pkg.debian.build
 
-# move to /usr/lib, this is location where NCCL examples look for nccl
-sudo cp /home/ubuntu/nccl/build/lib/libnccl* /usr/lib
-
+# todo(y): maybe these copies are not needed given env vars below
+# move to /usr/lib, this may be location where NCCL examples look for nccl
 # copy nccl 2.4.6, but the symlinks nccl and nccl.2 will point to nccl 2.3.7
+sudo cp /home/ubuntu/nccl/build/lib/libnccl* /usr/lib
 sudo cp /home/ubuntu/nccl/build/lib/libnccl* /usr/local/cuda/lib
 
 # (unneeded? Not clear if that path is used)
 export NCCL_HOME=~/nccl/build/lib
+export NCCL_ROOT_DIR=~/nccl/build/lib
 
 # testing
 cd ~
